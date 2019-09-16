9cda92935526ba2eb8487662ac18e5ffe2da507e
arcaster42 <tj.epperson070@gmail.com>
2019-09-16 09:57:40 +0900

    added uneven array support

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function arrayToHash(keyArray, valueArray, incStart) {
+function arrayToHash(keyArray, valueArray, incStart = 0) {
     let obj = {}
-    for (let i = 0; i < keyArray.length; i++) {
-        if (valueArray) {
-            obj[keyArray[i]] = valueArray[i]
-        } else {
-            obj[keyArray[i]] = incStart + i
+    if (!valueArray || keyArray.length === valueArray.length) {
+        for (let i = 0; i < keyArray.length; i++) {
+            if (valueArray) {
+                obj[keyArray[i]] = valueArray[i]
+            } else {
+                obj[keyArray[i]] = incStart + i
+            }
+        }
+    }
+    if (valueArray && valueArray.length < keyArray.length) {
+        let j = 0
+        for (let i = 0; i < keyArray.length; i++) {
+            if (valueArray[j]) {
+                obj[keyArray[i]] = valueArray[j]
+                j++
+            }
+            else {
+                j = 0
+                obj[keyArray[i]] = valueArray[j]
+                j++
+            }
         }
     }
     return obj
